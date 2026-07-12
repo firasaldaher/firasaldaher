966fd65550f9ed2e3d96f0b88eeeb4707df191f0
firasaldaher <52230392@students.liu.edu.lb>
2026-07-12 05:15:02 +0300

    Fix database include path in create_order.php

--- api/pos/create_order.php
+++ api/pos/create_order.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
-require_once __DIR__ . '/../api/config/database.php';
+require_once __DIR__ . '/../config/database.php';
 
 $database = new Database();
 $db = $database->getConnection();
